fd7bebaa09e620654497d8d7c3821fdf3bfb1bac
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-06-14 16:07:43 +0200

    adding html file

--- js/main.js
+++ js/main.js
@@ -1,29 +1,45 @@
 //creazione var legate all'user
 
 //creo una var per chiedere il nome all'utente
-let userName = (prompt(`Buongiorno mi dica il suo nome`));
+let userName = (prompt(`Buongiorno mi dica il suo nome e cognome`));
 console.log(userName);
+document.getElementById('user_name').innerHTML = userName
 
 //cro una var per chiedere l'età dell'utente
 let userAge = (parseInt(prompt(`benvenuto signor ${userName} , in base all'età ci sono degli sconti mi dica la sua`)));
 console.log(userAge);
+document.getElementById('user-age').innerHTML = userAge
 
 //creo una var per chiedere quanti km dovrà percorrere
-let kmToCover = (parseInt(prompt(`allora signor ${userName} ; quati km dovrà percorrere`)));
+let kmToCover = (parseInt(prompt(`allora signor ${userName} ; quati km dovrà percorrere ?`)));
 console.log(kmToCover);
+document.getElementById('km_tocover').innerHTML = kmToCover
 
 
-//! creazione cpsto biglietto
+//! creazione costo biglietto
 
 let PriceNoDiscount = (kmToCover * 0.21);
 console.log(PriceNoDiscount)
 
 let PriceDiscount =('')
-let Price =('')
+let Price =('') 
 
-if( userAge < 18){
-    PriceDiscount=((PriceNoDiscount * 20 ) / 100)
-    Price=( PriceNoDiscount - PriceDiscount )
+if (isNaN(userAge) == true){
+    alert(`l'età da lei indicata non è valida`)
+} else {
+    if( userAge < 18){
+        PriceDiscount=((PriceNoDiscount * 20 ) / 100)
+        Price=( PriceNoDiscount - PriceDiscount )
+    }else if(userAge > 65){
+        PriceDiscount=((PriceNoDiscount * 40 ) / 100)
+        Price=( PriceNoDiscount - PriceDiscount )
+    }else{
+        Price=PriceNoDiscount
+    }
 }
+
+
+
 console.log(PriceDiscount)
 console.log(Price)
+document.getElementById('price').innerHTML = Price
